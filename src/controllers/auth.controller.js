6be77538b9a213134e2e635d0fff3bbbeedfb7ba
AlvaroJBH <alvarobuenoh@gmail.com>
2025-12-06 02:24:53 -0500

    Token enviado en signin

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -34,7 +34,7 @@ class AuthController {
         maxAge: 1000 * 60 * 60,
       });
 
-      return res.status(200).json({ message: "Inicio de sesión exitoso" });
+      return res.status(200).json({ message: "Inicio de sesión exitoso", token });
     } catch (err) {
       next(err);
     }
